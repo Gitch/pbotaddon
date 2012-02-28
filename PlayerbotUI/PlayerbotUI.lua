ff7bca142bbb7e95372b7bd2e144938a40a2014e
Gitch <kryansmith1@windstream.net>
2012-02-28 17:22:23 -0500

    Alpha version client addon for Playerbot

---
+++ PlayerbotUI/PlayerbotUI.lua
@@ -0,0 +1,28 @@
+﻿
+SLASH_PlayerbotUI1, SLASH_PlayerbotUI2 = '/pbotmenu', '/pbm';
+local function handler(msg, editbox)
+ if msg == 'go2' then
+  PlayerbotUIForm1:Show();
+ else
+  PlayerbotUIForm:Show();
+ end
+end
+SlashCmdList["PlayerbotUI"] = handler;
+
+
+function PlayerbotUIFormEvent(argl)
+    if(event == "VARIABLES_LOADED") then
+        local count,label;
+        if(PlayerbotUISavedVar == nil) then
+            PlayerbotUISavedVar = {};
+        end
+        count = PlayerbotUISavedVar["count"];
+        if(count == nil) then
+            count = 0;
+        end
+        label = getglobal("PlayerbotUIForm".."CountLabel".."Label");
+        label:SetText("This form has been loaded "..count.." time(s)");
+        PlayerbotUISavedVar["count"] = count + 1;
+    end
+
+end            
---
+++ README.txt
@@ -0,0 +1,3 @@
+Client addon for Blueboy's Playerbot for Mangos
+
+
